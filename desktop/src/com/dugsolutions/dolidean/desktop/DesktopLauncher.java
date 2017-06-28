289fd514b71ada3a4842d494ee5b6f0c40b3a27f
Douglas Selph <douglasselph@gmail.com>
2017-06-28 13:11:55 -0500

    initial checkin

---
+++ desktop/src/com/dugsolutions/dolidean/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.dugsolutions.dolidean.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dugsolutions.dolidean.MyApplication;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+		config.height = 700;
+		new LwjglApplication(new MyApplication(), config);
+	}
+}
